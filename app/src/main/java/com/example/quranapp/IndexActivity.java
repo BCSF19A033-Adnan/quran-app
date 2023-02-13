dda9f6a3af6fb7545775b877fd1f9bd703dde628
BCSF19A033-Adnan <116968748+BCSF19A033-Adnan@users.noreply.github.com>
2023-02-13 23:23:43 +0500

    Minor fixes and implement IndexActivity

--- app/src/main/java/com/example/quranapp/IndexActivity.java
+++ app/src/main/java/com/example/quranapp/IndexActivity.java
@@ -1,14 +1,46 @@
 package com.example.quranapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IndexActivity extends AppCompatActivity {
 
+    List<String> parahList = new ArrayList<>();
+    List<String> surrahList = new ArrayList<>();
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        Intent intent = getIntent();
+        String searchCategory = intent.getStringExtra("category");
+
+        initializeDataInList();
+        recyclerView = findViewById(R.id.indexView);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new IndexAdapter(getApplicationContext(), parahList) ;
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initializeDataInList() {
+        for(int i=1; i<=30; i++)
+        {
+            String st = "Parah ";
+            st = st+i;
+            parahList.add(st);
+        }
     }
 }
